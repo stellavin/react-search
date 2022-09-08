9aa7423e8c391ff9382093b3a980f1e70f64e30b
Stella Sikhila <stellavinmaris@gmail.com>
2022-09-08 16:19:01 +0300

    removed class

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ function App() {
 
   return (
     <div className="dark:bg-gray-900 m-0 w-full min-h-screen">
-      <div class="container mx-auto px-4">
+      <div className="container mx-auto px-4">
         <h3 className="text-center text-xl font-medium leading-6 text-gray-100 pt-10 pb-10">
-          Search your Movies
+          Search Your Movies
         </h3>
         <div className="items-center">
           <SearchBar onSearch={onSearch} />
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,22 +2,36 @@ import React from "react";
 
 function Card({ movie }) {
   return (
-    <div class="bg-white shadow-lg border-gray-100 max-h-50 border sm:rounded-3xl flex space-x-8 m-5 mb-5">
-      <div class="h-4/6 overflow-visible w-3/6">
+    <div className="bg-white shadow-lg border-gray-100 max-h-50 border sm:rounded-3xl flex space-x-8 m-5 mb-5">
+      <div className="h-4/6 overflow-visible w-3/6">
         <img
-          class="rounded-3xl shadow-lg h-50"
+          className="rounded-3xl shadow-lg h-50"
           src={movie.thumbnail}
           alt={movie.name}
         />
+        <div className="p-4">
+          <h2 className="text-l font-bold">Genres</h2>
+          {movie.genres.map((item, index) => (
+            <div className="text-sm text-gray-800">
+              {index + 1}. {item}
+            </div>
+          ))}
+        </div>
       </div>
-      <div class="flex flex-col w-1/2 p-2 ">
-        <div class="flex justify-between items-start">
-          <h2 class="text-l font-bold">{movie.name}</h2>
+      <div className="flex flex-col w-1/2 p-2 ">
+        <div className="flex justify-between items-start">
+          <h2 className="text-l font-bold">{movie.name}</h2>
         </div>
         <div>
-          <div class="text-sm text-gray-400">Duration</div>
-          <div class="text-sm text-gray-800">{movie.duration}</div>
-          <div class="text-sm text-gray-500">{movie.description}</div>
+          <div className="text-sm text-gray-400 mt-2">Duration</div>
+          <div className="text-sm text-gray-800">
+            {
+              new Date(movie.duration * 1000)
+                .toUTCString()
+                .match(/(\d\d:\d\d:\d\d)/)[0]
+            }
+          </div>
+          <div className="text-sm text-gray-500 mt-2">{movie.description}</div>
         </div>
       </div>
     </div>
